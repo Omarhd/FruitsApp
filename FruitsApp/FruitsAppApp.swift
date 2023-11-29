c12bc4afbdc4fd91eb595345a916c6c4dc7f6551
Omar Abdulrahman <omeralhadu@icloud.com>
2023-11-29 15:12:58 +0400

    Initial Commit

---
+++ FruitsApp/FruitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsAppApp.swift
+//  FruitsApp
+//
+//  Created by Omar Abdulrahman on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
